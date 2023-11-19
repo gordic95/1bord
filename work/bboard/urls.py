ce3e8b3d97b797f33c49e08c7bc5279c6c6adcf1
oleg gordov <irkstripman@yandex.ru>
2023-11-19 18:32:22 +0300

    1
    1

---
+++ work/bboard/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, CreateComment, AuthorDetailView
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    path('', PostListView.as_view(), name='posts'),
+    path('<int:pk>', PostDetailView.as_view(), name='post_detail'),
+    path('create/', PostCreateView.as_view(), name='post_create'),
+    path('<int:pk>/update/', PostUpdateView.as_view(), name='post_update'),
+    path('<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
+    path('<int:pk>/create_comment', CreateComment.as_view(), name='create_comment'),
+    path('author/<int:pk>', AuthorDetailView.as_view(), name='author_detail')
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
